72898e6af180b9e9ac48a8c5226ea044639eb14a
Himmelt <master@void-3.cn>
2016-05-30 21:25:07 +0800

    update project.

--- src/main/java/io/github/himmelt/limitime/Events.java
+++ src/main/java/io/github/himmelt/limitime/Events.java
@@ -1,6 +1,7 @@
 package io.github.himmelt.limitime;
 /* Created by Kami on 2016/5/29.*/
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -12,25 +13,65 @@ import java.util.List;
 import java.util.logging.Logger;
 
 public class Events implements Listener {
+
     public static Logger logger = Logger.getLogger("Limitime");
 
     @EventHandler
     public void onPlayerAction(PlayerInteractEvent event) {
-        final Player player = event.getPlayer();
-        logger.info("interact!!");
-        logger.info(player.getDisplayName());
-        logger.info(player.getItemInHand().toString());
-        logger.info("ItemMeta:" + player.getItemInHand().hasItemMeta());
-        if (player.getItemInHand() != null && player.getItemInHand().hasItemMeta()) {
+        logger.info("[Limitime]:onPlayerAction");
+        Player player = event.getPlayer();
+        if (player.getItemInHand().hasItemMeta()) {
             ItemStack itemStack = player.getItemInHand();
             ItemMeta itemMeta = itemStack.getItemMeta();
+            logger.info("[Limitime]:hasItemMeta" + itemMeta);
             List<String> itemLore = itemMeta.getLore();
-            itemLore.add("&a&lThis is a test string! &e&lif you interact with sth.");
-            //itemMeta.setLore(itemLore);
-            itemMeta.setLore(itemLore);
-            itemStack.setItemMeta(itemMeta);
-            player.setItemInHand(itemStack);
-            logger.info("replace ok!");
+            String lore;
+            for (int i = 0; i < itemLore.size(); i++) {
+                lore = itemLore.get(i);
+                logger.info("[Limitime]:lore-" + i + lore);
+                if (lore.contains("lit:duration:")) {
+                    logger.info("[Limitime]:duration");
+                    long time_now = System.currentTimeMillis() / 60000;//单位：小时
+                    if (lore.contains(":limitime:")) {
+                        long limitime = getLimitime(lore);
+                        logger.info("[Limitime]:time" + time_now + ":" + limitime);
+                        if (limitime < time_now) {
+                            logger.info("[Limitime]:timeout");
+                            player.setItemInHand(new ItemStack(Material.AIR));
+                        }
+                    } else {
+                        logger.info("[Limitime]:newlimit");
+                        long duration = getDuration(lore);
+                        lore = lore + ":limitime:" + Long.toString(time_now + duration);
+                        logger.info("[Limitime]:newlore-" + lore);
+                        itemLore.set(i, lore);
+                        itemMeta.setLore(itemLore);
+                        itemStack.setItemMeta(itemMeta);
+                        player.setItemInHand(itemStack);
+                    }
+                    break;
+                }
+            }
         }
+    }
+
+    private long getDuration(String lore) {
+        long duration = 0;
+        try {
+            duration = Long.parseLong(lore.substring(lore.indexOf("lit:duration:") + 13));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return duration;
+    }
+
+    private long getLimitime(String lore) {
+        long limitime = 0;
+        try {
+            limitime = Long.parseLong(lore.substring(lore.indexOf(":limitime:") + 10));
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
+        }
+        return limitime;
     }
 }
