d90f1ed47dbbf6dcc1811f24db31312a4da83e41
Himmelt <master@void-3.cn>
2016-06-02 12:44:23 +0800

    add command /limitime & deploy v0.1

--- src/main/java/io/github/himmelt/limitime/Plugin.java
+++ src/main/java/io/github/himmelt/limitime/Plugin.java
@@ -1,6 +1,8 @@
 package io.github.himmelt.limitime;
 /* Created by Kami on 2016/5/27.*/
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Plugin extends JavaPlugin {
@@ -23,5 +25,21 @@ public final class Plugin extends JavaPlugin {
     public void onDisable() {
         this.getServer().getScheduler().cancelAllTasks();
         Log.info("Limitime is disabled!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("limitime")) {
+            //如果玩家输入了/basic则执行如下内容...
+            sender.sendMessage("§a========§e§l物品限时插件(v0.1)§r§a========");
+            sender.sendMessage("§a===§ehttps://github.com/Himmelt/Limitime§r§a===");
+            sender.sendMessage("§e添加时限lore的格式为:");
+            sender.sendMessage("§a§l[duration:+1d+2m+3h]");
+            sender.sendMessage("§6时限部分可以为:");
+            sender.sendMessage("§a§l+1m,+1m+1d,+1d+1m+1h,+1d+1d,+1m+1m+1m");
+            sender.sendMessage("§a§l+1h+1h+1d,§r§6全部为叠加状态.");
+            return true;
+        }
+        return false;
     }
 }
